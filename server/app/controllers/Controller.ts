9ce19588b8892dbb05911da2e35e5dc975b8dbbc
B Roberts <ben.roberts@massive.co>
2019-01-26 14:11:01 +0100

    further refactoring in Controller

--- server/app/controllers/Controller.ts
+++ server/app/controllers/Controller.ts
@@ -4,6 +4,7 @@ const { OAuth2Client } = require('google-auth-library');
 const CLIENT_ID = '1076782358657-ek02t3rpa2e7e1kll0pntvl7li7jo827.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
 import { Request, Response } from 'express';
+import Mysql from '../util/Mysql';
 
 export abstract class Controller {
 
@@ -12,8 +13,12 @@ export abstract class Controller {
 	protected userId: string = '';
 	private callback!: Function;
 
-	protected constructor() {
+	public constructor() {
 		this.onTokenValidated = this.onTokenValidated.bind(this);
+		this.executeQuery = this.executeQuery.bind(this);
+		this.getQuery = this.getQuery.bind(this);
+		this.onQueryExecuted = this.onQueryExecuted.bind(this);
+		this.execute = this.execute.bind(this);
 	}
 
 	protected validateToken(callback: Function) {
@@ -25,6 +30,28 @@ export abstract class Controller {
 			},
 			this.onTokenValidated);
 		return true;
+	}
+
+	public execute(req: Request, res: Response, next: Function): void {
+		this.req = req;
+		this.res = res;
+		this.validateToken(this.executeQuery);
+	}
+
+	executeQuery() {
+		Mysql.createConnection().query(this.getQuery(), this.onQueryExecuted);
+	}
+
+	abstract getQuery(): string;
+
+	onQueryExecuted(err: Error, rows: ) { // TODO can't use type any, must declare type for rows returned from query
+		if (err) {
+			this.res.header('Access-Control-Allow-Origin', '*');
+			this.res.status(500).send({ error: err });
+			return;
+		}
+		this.res.header('Access-Control-Allow-Origin', '*');
+		this.res.json(rows);
 	}
 
 	onTokenValidated(err: Error | null, login: LoginTicket) {
--- server/app/controllers/InsertLexemePair.ts
+++ server/app/controllers/InsertLexemePair.ts
@@ -1,48 +1,18 @@
-import { Request, Response } from 'express';
 import { Controller } from './Controller';
-import Mysql from '../util/Mysql';
 import { StringUtils } from '../util/StringUtils';
 
 export default class InsertLexemePair extends Controller {
 
-	constructor() {
-		super();
-		this.executeQuery = this.executeQuery.bind(this);
-		this.getQuery = this.getQuery.bind(this);
-		this.onQueryExecuted = this.onQueryExecuted.bind(this);
-		this.execute = this.execute.bind(this);
-	}
-
-	public execute(req: Request, res: Response, next: Function): void {
-		this.req = req;
-		this.res = res;
-		this.validateToken(this.executeQuery);
-	}
-
-	executeQuery() {
-		Mysql.createConnection().query(this.getQuery(this.req), this.onQueryExecuted);
-	}
-
-	onQueryExecuted(err: Error, rows: any) {
-		if (err) {
-			this.res.header('Access-Control-Allow-Origin', '*');
-			this.res.status(500).send({ error: err });
-			return;
-		}
-		this.res.header('Access-Control-Allow-Origin', '*');
-		this.res.json(rows);
-	}
-
-	getQuery(req: Request): string {
-		const wordType = StringUtils.formatEnum(req.body.wordType);
-		const phraseType = StringUtils.formatEnum(req.body.phraseType);
-		const type = StringUtils.formatEnum(req.body.type);
-		const gender = StringUtils.formatEnum(req.body.czGender);
-		const verbAspect = StringUtils.formatEnum(req.body.czVerbAspect);
-		const notes = req.body.notes || '';
-		const czNotes = req.body.czNotes || '';
-		const enNotes = req.body.enNotes || '';
-		const ip = req.ip;
+	getQuery(): string {
+		const wordType = StringUtils.formatEnum(this.req.body.wordType);
+		const phraseType = StringUtils.formatEnum(this.req.body.phraseType);
+		const type = StringUtils.formatEnum(this.req.body.type);
+		const gender = StringUtils.formatEnum(this.req.body.czGender);
+		const verbAspect = StringUtils.formatEnum(this.req.body.czVerbAspect);
+		const notes = this.req.body.notes || '';
+		const czNotes = this.req.body.czNotes || '';
+		const enNotes = this.req.body.enNotes || '';
+		const ip = this.req.ip;
 
 		let czWord = '';
 		let czPhrase = '';
@@ -50,16 +20,16 @@ export default class InsertLexemePair extends Controller {
 		let enPhrase = '';
 
 		if (type === 'WORD') {
-			czWord = req.body.czText;
-			enWord = req.body.enText;
+			czWord = this.req.body.czText;
+			enWord = this.req.body.enText;
 		} else {
-			czPhrase = req.body.czText;
-			enPhrase = req.body.enText;
+			czPhrase = this.req.body.czText;
+			enPhrase = this.req.body.enText;
 		}
 
 		return `CALL insertLexemePair('${czWord}','${enWord}','${czPhrase}','${enPhrase}','${wordType}','${phraseType}
-		','${type}','${gender}','${verbAspect}','${notes}','${enNotes}','${czNotes}', '${ip}', '${this.userId}
-		', @insert_id);`;
+			','${type}','${gender}','${verbAspect}','${notes}','${enNotes}','${czNotes}', '${ip}', '${this.userId}
+			', @insert_id);`;
 	}
 
 }
