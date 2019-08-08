c5aec5deb92e78b7b71598128a8673c9371bd4eb
B Roberts <ben.roberts@massive.co>
2019-08-08 14:12:19 +0200

    splash screen

--- client/src/components/PageLayer.tsx
+++ client/src/components/PageLayer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Settings from './settings/Settings';
 import Add from './add/Add';
 import AddView from '../enum/AddView';
@@ -10,74 +10,82 @@ import Recent from './recent/Recents';
 import Edit from './edit/Edit';
 import Delete from './Delete';
 import Test from './tests/Test';
+import SplashScreeen from './SplashScreeen';
 
 export default class PageLayer extends Component {
 
 	render() {
 		return (
 			<div className="pageLayer">
+				<Switch>
 
-				<Route
-					path="/settings"
-					render={() => <Settings/>}
-				/>
+					<Route
+						path="/settings"
+						render={() => <Settings/>}
+					/>
 
-				<Route
-					path="/add/en"
-					render={() => <Add view={AddView.ENGLISH} />}
-				/>
+					<Route
+						path="/add/en"
+						render={() => <Add view={AddView.ENGLISH} />}
+					/>
 
-				<Route
-					path="/add/cz"
-					render={() => <Add view={AddView.CZECH} />}
-				/>
+					<Route
+						path="/add/cz"
+						render={() => <Add view={AddView.CZECH} />}
+					/>
 
-				<Route
-					path="/add/confirm"
-					render={() => <Add view={AddView.CONFIRM} />}
-				/>
+					<Route
+						path="/add/confirm"
+						render={() => <Add view={AddView.CONFIRM} />}
+					/>
 
-				<Route
-					path="/tests"
-					render={() => <Tests/>}
-				/>
+					<Route
+						path="/tests"
+						render={() => <Tests/>}
+					/>
 
-				<Route
-					path="/test/:type/:length/:languageToTest"
-					render={(props) =>
-						<Test
-							type={props.match.params.type}
-							length={props.match.params.length}
-							languageToTest={props.match.params.languageToTest}
-						/>}
-				/>
+					<Route
+						path="/test/:type/:length/:languageToTest"
+						render={(props) =>
+							<Test
+								type={props.match.params.type}
+								length={props.match.params.length}
+								languageToTest={props.match.params.languageToTest}
+							/>}
+					/>
 
-				<Route
-					path="/search"
-					render={() => <Search/>}
-				/>
+					<Route
+						path="/search"
+						render={() => <Search/>}
+					/>
 
-				<Route
-					path="/recent"
-					render={() => <Recent/>}
-				/>
+					<Route
+						path="/recent"
+						render={() => <Recent/>}
+					/>
 
-				<Route
-					path="/edit/:czId/:enId"
-					render={(props) =>
-						<Edit
-							enId={props.match.params.enId}
-							czId={props.match.params.czId}
-							view={AddView.ENGLISH}
-						/>}
-				/>
+					<Route
+						path="/edit/:czId/:enId"
+						render={(props) =>
+							<Edit
+								enId={props.match.params.enId}
+								czId={props.match.params.czId}
+								view={AddView.ENGLISH}
+							/>}
+					/>
 
-				<Route
-					path="/delete"
-					render={() =>
-						<Delete/>}
-				/>
+					<Route
+						path="/delete"
+						render={() =>
+							<Delete/>}
+					/>
 
+					<Route
+						path="/"
+						render={() =>
+							<SplashScreeen />}
+					/>
+				</Switch>
 			</div>
 		);
 	}
---
+++ client/src/components/SplashScreeen.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export default class SplashScreeen extends Component {
+	render() {
+		return (
+			<div id="splashScreen">
+				<div id="middleContainer">
+					<div id="appName">CZAPP</div>
+					<div id="tagLine">Personal Czech - English training dictionary</div>
+				</div>
+			</div>);
+
+	}
+}
