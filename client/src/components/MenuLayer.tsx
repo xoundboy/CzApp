121bf2e54b6b5a72dde5eebe18e397cf1a7141d5
B Roberts <ben.roberts@massive.co>
2019-08-10 09:26:44 +0200

    selecting add from menu clears the existing lexeme

--- client/src/components/MenuLayer.tsx
+++ client/src/components/MenuLayer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Component } from 'react';
-import { AppContextConsumer } from 'AppContext';
+import { AppContextConsumer, IAppContext } from 'AppContext';
 
 export interface IMenuLayerState {
 	navIsOpen: Boolean;
@@ -9,19 +9,21 @@ export interface IMenuLayerState {
 
 export default class MenuLayer extends Component<object, IMenuLayerState> {
 
+	context: IAppContext;
+
 	constructor(props: object) {
 		super(props);
 		this.state = {
 			navIsOpen: false
 		};
-		this.onMenuVisibilityChanged = this.onMenuVisibilityChanged.bind(this);
+		this.toggleVisibility = this.toggleVisibility.bind(this);
 	}
 
 	getClassName() {
 		return 'menuLayer' + (this.state.navIsOpen ? ' open' : ' closed');
 	}
 
-	onMenuVisibilityChanged() {
+	toggleVisibility() {
 		this.setState({navIsOpen: !this.state.navIsOpen});
 	}
 
@@ -39,9 +41,10 @@ export default class MenuLayer extends Component<object, IMenuLayerState> {
 		return (
 			<AppContextConsumer>
 				{(context) => {
+					this.context = context;
 					return (
 						<nav className="menuItems">
-							{this.renderItem(`/add/${context.inputLanguage}`, context.dictionary.MENULABEL_ADD)}
+							{this.renderItem(`/add/${context.inputLanguage}`, context.dictionary.MENULABEL_ADD, true)}
 							{this.renderItem('/recent', context.dictionary.MENULABEL_RECENT)}
 							{this.renderItem('/search', context.dictionary.MENULABEL_SEARCH)}
 							{this.renderItem('/tests', context.dictionary.MENULABEL_TESTS)}
@@ -54,11 +57,23 @@ export default class MenuLayer extends Component<object, IMenuLayerState> {
 		);
 	}
 
-	renderItem(path: string, label: string) {
-		return (<Link to={path} onClick={this.onMenuVisibilityChanged}>{label}</Link>);
+	renderItem(path: string, label: string, clearLexemePair: boolean = false) {
+
+		if (clearLexemePair)
+			return (
+				<Link
+					to={path}
+					onClick={() => {
+						this.context.onClearDataButtonClicked();
+						this.toggleVisibility();
+					}}
+				>{label}
+				</Link>);
+
+		return (<Link to={path} onClick={this.toggleVisibility}>{label}</Link>);
 	}
 
 	renderNavButton() {
-		return (<div className="menuButton" onClick={this.onMenuVisibilityChanged} />);
+		return (<div className="menuButton" onClick={this.toggleVisibility} />);
 	}
 }
